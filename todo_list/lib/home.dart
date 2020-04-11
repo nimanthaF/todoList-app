51f4585c721d6c8173d9909f20323e4c363efe4b
nimanthaF <mimanthafernando95@gmail.com>
2020-04-11 09:37:56 +0530

    reorderable view done

--- todo_list/lib/home.dart
+++ todo_list/lib/home.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+List<String> item=["item1","item2","item3","item4","item"];
+
 class App extends StatefulWidget {
   @override
   _AppState createState() => _AppState();
 }
 
 class _AppState extends State<App> {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +20,27 @@ class _AppState extends State<App> {
             color: Colors.black,
           ),
         ),
+      ),
+      body: ReorderableListView(
+        onReorder: (oldIndex,newIndex){
+          setState(() {
+            if(oldIndex < newIndex){
+              newIndex -= 1;
+            }
+            var getReplacedWidget = item.removeAt(oldIndex);
+            item.insert(newIndex, getReplacedWidget);
+        },);},
+        children: <Widget>[
+          for(final value in item)
+            Text(
+              value,
+              key: Key(value),
+              style: TextStyle(
+                fontSize: 22.0
+              ),
+              )
+          
+        ],
       ),
     );
   }
