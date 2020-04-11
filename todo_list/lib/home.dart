8b8b24e8d78cf8f49e2857be0b8cefe50972b7dd
nimanthaF <mimanthafernando95@gmail.com>
2020-04-11 09:06:11 +0530

    after making the appbar

---
+++ todo_list/lib/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "to-do list",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
